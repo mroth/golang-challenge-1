7584ba63b47e45d7c7670689a933104b4f081cc4
Matthew Rothenberg <mrothenberg@gmail.com>
2015-03-06 22:56:38 -0500

    handle output formatting

--- drum-machine-go/drum.go
+++ drum-machine-go/drum.go
@@ -2,6 +2,11 @@
 // See golang-challenge.com/go-challenge1/ for more information
 package drum
 
+import (
+	"bytes"
+	"fmt"
+)
+
 // Pattern is the high level representation of the drum pattern contained in
 // a .splice file.
 type Pattern struct {
@@ -10,15 +15,57 @@ type Pattern struct {
 	Tracks  []*Track
 }
 
+func (p *Pattern) String() string {
+	var output bytes.Buffer
+	output.WriteString(p.versionString())
+	output.WriteString(p.tempoString())
+
+	for _, t := range p.Tracks {
+		output.WriteString(t.String())
+	}
+	return output.String()
+}
+
+func (p *Pattern) versionString() string {
+	return fmt.Sprintf("Saved with HW Version: %s\n", p.Version)
+}
+
+func (p *Pattern) tempoString() string {
+	return fmt.Sprintf("Tempo: %v\n", p.Tempo)
+}
+
 // Track is the high level representation of a single instrument within a
 // a .splice file Pattern.
 //
-// It represents timing information for
+// It represents timing information.
 type Track struct {
 	ID    uint8
 	Name  string   // the name of the instrument, or "sample"
 	Beats [16]Beat // grid of beat step information in 4/4 time
 }
 
+func (t *Track) String() string {
+	return fmt.Sprintf("(%d) %s\t%s\n", t.ID, t.Name, t.beatGrid())
+}
+
+func (t *Track) beatGrid() string {
+	var output bytes.Buffer
+	output.WriteString("|")
+	for i, b := range t.Beats {
+		output.WriteString(b.String())
+		if (i+1)%4 == 0 {
+			output.WriteString("|")
+		}
+	}
+	return output.String()
+}
+
 // Beat represents whether an instrument is "played" for a particular step
 type Beat bool
+
+func (b Beat) String() string {
+	if b {
+		return "x"
+	}
+	return "-"
+}
